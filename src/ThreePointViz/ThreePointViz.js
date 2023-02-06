98f4a31c8d7a69e4cc52652c8313976de008cf42
Thomas Hodges <thodges@javascriptonline.org>
2023-02-06 15:34:21 -0800

    cylinder

---
+++ src/ThreePointViz/ThreePointViz.js
@@ -0,0 +1,23 @@
+import { Canvas } from "react-three-fiber";
+// import Controls from "./Controls";
+
+const ThreePointViz = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      {/* <Controls /> */}
+      <ambientLight color="#fff" intensity={0.1} />
+      <hemisphereLight
+        color="#fff"
+        skyColor="#ffffbb"
+        groundColor="#080820"
+        intensity={1}
+      />
+      <mesh position={[0, 0, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
+        <cylinderBufferGeometry attach="geometry" args={[0.5, 0.5, 0.15, 32]} />
+        <meshStandardMaterial attach="material" color="#fff" />
+      </mesh>
+    </Canvas>
+  );
+};
+
+export default ThreePointViz;
