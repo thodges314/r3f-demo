2c9b345ee2c0281e693ee7b234613969cfa2360c
Thomas Hodges <thodges@javascriptonline.org>
2023-02-06 15:22:42 -0800

    starting step - raw app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import * from React from 'react';
+import ThreePointViz from "./ThreePointViz/ThreePointViz";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="vis-container">
+        <ThreePointViz />
+      </div>
     </div>
   );
 }
-
-export default App;
