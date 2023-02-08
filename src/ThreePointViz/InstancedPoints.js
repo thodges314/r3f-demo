b1cda1fe26f747d2b0fd5cfdcce97ff4fe4f54d2
Thomas Hodges <thodges@javascriptonline.org>
2023-02-07 18:25:56 -0800

    animations - requires older react spring version

--- src/ThreePointViz/InstancedPoints.js
+++ src/ThreePointViz/InstancedPoints.js
@@ -1,29 +1,37 @@
 import { useEffect, useRef } from "react";
 import { Object3D } from "three";
-import { useLayout } from "./layouts";
+import { useAnimatedLayout } from "./layouts";
 
 const scratchObject3D = new Object3D();
+
+const updateInstancedMeshMatricies = ({ mesh, data }) => {
+  if (!mesh) return;
+  for (let i = 0; i < data.length; ++i) {
+    const { x, y, z } = data[i];
+
+    scratchObject3D.position.set(x, y, z);
+    scratchObject3D.rotation.set(0.5 * Math.PI, 0, 0);
+    scratchObject3D.updateMatrix();
+    mesh.setMatrixAt(i, scratchObject3D.matrix);
+  }
+  mesh.instanceMatrix.needsUpdate = true;
+};
 
 const InstancedPoints = ({ data, layout }) => {
   const meshRef = useRef();
   const numPoints = data.length;
 
-  useLayout({ data, layout });
+  useAnimatedLayout({
+    data,
+    layout,
+    onFrame: () =>
+      updateInstancedMeshMatricies({ mesh: meshRef.current, data }),
+  });
 
-  useEffect(() => {
-    const mesh = meshRef.current;
-
-    for (let i = 0; i < numPoints; i++) {
-      const { x, y, z } = data[i];
-
-      scratchObject3D.position.set(x, y, z);
-      scratchObject3D.rotation.set(0.5 * Math.PI, 0, 0);
-      scratchObject3D.updateMatrix();
-      mesh.setMatrixAt(i, scratchObject3D.matrix);
-    }
-
-    mesh.instanceMatrix.needsUpdate = true;
-  }, [numPoints, data, layout]);
+  useEffect(
+    () => updateInstancedMeshMatricies({ mesh: meshRef.current, data }),
+    [data, layout]
+  );
 
   return (
     <instancedMesh
--- src/ThreePointViz/layouts.js
+++ src/ThreePointViz/layouts.js
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useSpring } from "react-spring/three";
 
 const gridLayout = (data) => {
   const numPoints = data.length;
   const numCols = Math.ceil(Math.sqrt(numPoints));
   const numRows = numCols;
 
-  for (let i = 0; i < numPoints; i++) {
+  for (let i = 0; i < numPoints; ++i) {
     const datum = data[i];
     const col = (i % numCols) - numCols / 2;
     const row = Math.floor(i / numCols) - numRows / 2;
@@ -40,4 +41,58 @@ export const useLayout = ({ data, layout = "grid" }) => {
       }
     }
   }, [data, layout]);
+};
+
+const useSourceTargetLayout = ({ data, layout }) => {
+  // prep for animation by stouring source
+  useEffect(() => {
+    for (let i = 0; i < data.length; ++i) {
+      data[i].sourceX = data[i].x || 0;
+      data[i].sourceY = data[i].y || 0;
+      data[i].sourceZ = data[i].z || 0;
+    }
+  }, [data, layout]);
+
+  // run layout
+  useLayout({ data, layout });
+
+  //store target
+  useEffect(() => {
+    for (let i = 0; i < data.length; ++i) {
+      data[i].targetX = data[i].x || 0;
+      data[i].targetY = data[i].y || 0;
+      data[i].targetZ = data[i].z || 0;
+    }
+  }, [data, layout]);
+};
+
+const interpolateSourceTarget = (data, progress) => {
+  for (let i = 0; i < data.length; ++i) {
+    data[i].x =
+      data[i].sourceX + (data[i].targetX - data[i].sourceX) * progress;
+    data[i].y =
+      data[i].sourceY + (data[i].targetY - data[i].sourceY) * progress;
+    data[i].z =
+      data[i].sourceZ + (data[i].targetZ - data[i].sourceZ) * progress;
+  }
+};
+
+export const useAnimatedLayout = ({ data, layout, onFrame }) => {
+  // compute layout remembering initial position as source and end position as target
+  useSourceTargetLayout({ data, layout });
+
+  // do the actual animation when layout changes
+  const prevLayout = useRef(layout);
+  useSpring({
+    animationProgress: 1,
+    from: { animationProgress: 0 },
+    reset: layout !== prevLayout.current,
+    onFrame: ({ animationProgress }) => {
+      // interpolate based on progress
+      interpolateSourceTarget(data, animationProgress);
+      // callback to indicate that data has updated
+      onFrame({ animationProgress });
+    },
+  });
+  prevLayout.current = layout;
 };
